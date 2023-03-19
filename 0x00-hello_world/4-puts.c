1835d14fce5d3bef2e59dee9ac1a727060df665c
AHMEDSEID1 <kedi3340@gmail.com>
2023-03-19 18:03:07 +0000

    put.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry poit
+ *return: always 0 (successs)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
