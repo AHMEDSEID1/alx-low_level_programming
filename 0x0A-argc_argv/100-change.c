0a0303ac740c4dc65fbb4830011bc44dbc79e77f
AHMEDSEID1 <kedi3340@gmail.com>
2023-06-14 20:22:45 +0000

    correction for mixed declaration

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,26 +10,26 @@
  */
 int main(int argc, char *argv[])
 {
+	int cents, coins, i;
+	int denominations[] = {25, 10, 5, 2, 1};
+	int size = sizeof(denominations) / sizeof(int);
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]);
-
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	int coins = 0;
+	coins = 0;
 
-	int denominations[] = {25, 10, 5, 2, 1};
-	int size = sizeof(denominations) / sizeof(int);
-
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		coins += cents / denominations[i];
 		cents %= denominations[i];
