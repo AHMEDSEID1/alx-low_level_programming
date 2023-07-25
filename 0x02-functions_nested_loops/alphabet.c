b5d70e72d82db13e8eb29e2ae7a6ad06f1ee6680
AHMEDSEID1 <kedi3340@gmail.com>
2023-07-25 13:39:27 +0000

    remove

---
+++ 0x02-functions_nested_loops/alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/*
+   *main - check the code
+   *Return - always sucess 0
+   */
+void print_alphabet(void)
+{
+	int c;
+		for (c = 'A'; c <= 'Z'; C++)
+	
+		{
+			_putchar(c);
+
+		}
+		_putchar("\n");
+		return (0);
+}
+
+
--- 0x15-file_io/README.md
+++
@@ -1,7 +0,0 @@
-C - File I/O
-concepts worth cheaking
-man open
-man 2 write
-man close
-man 2 read
-
